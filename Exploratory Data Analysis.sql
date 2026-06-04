34e1f3c886420f80688da2898d76ccb4c1161402
kay <71146091+KayT98@users.noreply.github.com>
2026-06-03 22:11:25 -0500

    Update Exploratory Data Analysis.sql

--- Exploratory Data Analysis.sql
+++ Exploratory Data Analysis.sql
@@ -70,3 +70,35 @@ order by 1 asc
 select `month`, total_off, sum(total_off) over(order by `month`) as rolling_Total
 from rolling_total;
 
+--
+
+select company, sum(total_laid_off)
+from layoffs_staging2
+group by company
+order by 2 desc;
+
+-- 
+select company, year(`date`), sum(total_laid_off)
+from layoffs_staging2
+group by company, year(`date`)
+order by 3 desc;
+
+-- CTE
+
+with company_year (company, years, total_laid_off) as
+(
+select company, year(`date`), sum(total_laid_off)
+from layoffs_staging2
+group by company, year(`date`)
+), company_year_rank as
+(select *, dense_rank() over(partition by years order by total_laid_off desc) as ranking
+from company_year
+where years is not null
+)
+
+select *
+from company_year_rank
+where ranking <= 5;
+
+from rolling_total;
+
